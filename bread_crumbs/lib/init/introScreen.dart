3d93df3fbd4bb10df928ecd0374bb41370fe4ca4
Mason Caird <masoncaird@Masons-MBP.whitworth.edu>
2020-09-13 11:02:20 -0700

    Regenerated Files

---
+++ bread_crumbs/lib/init/introScreen.dart
@@ -0,0 +1,44 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../authenticate/signin.dart';
+import '../screens/listofLists.dart';
+
+// Help from: https://medium.com/firebase-tips-tricks/how-to-use-firebase-authentication-in-flutter-50e8b81cb29f
+
+class IntroScreen extends StatefulWidget {
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Check if user is already signed in
+    FirebaseAuth.instance.currentUser().then((res) {
+      print(res);
+      if (res != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => ListScreen(uid: res.uid)),
+        );
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SigninScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('images/globe.png'),
+      ),
+    );
+  }
+}
--- breadcrumbs/android/app/src/main/kotlin/com/example/breadcrumbs/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.breadcrumbs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
